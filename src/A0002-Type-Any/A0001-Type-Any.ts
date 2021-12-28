e64815524fefd8effb41900be006ad38a82641da
FabricioAnelli <fabricio_c_anelli@hotmail.com>
2021-12-28 13:23:15 -0300

    #02 Type Any

---
+++ src/A0002-Type-Any/A0001-Type-Any.ts
@@ -0,0 +1,9 @@
+// Any - A variável pode ter qualquer valor
+
+function showMessage(msg: any){
+  return msg;
+}
+
+console.log(showMessage([1, 2, 3]));
+console.log(showMessage('Olá'));
+console.log(showMessage(1));
